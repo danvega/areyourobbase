e915e9216d5d0eacd84dfe02557d4a2f2f19afae
Dan Vega <dan@techelevator.com>
2020-01-17 16:03:57 -0500

    adding octocat svg thanks to Sarah Drasner!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
+  <AppGithubCorner />
   <section
     v-for="choice in choices"
     :key="choice.id"
     :id="choice.id"
-    :style="{'background-color': choice.background}"
+    :style="{ 'background-color': choice.background }"
   >
     <div class="choice">
       <div class="question">{{ choice.question }}</div>
@@ -11,73 +12,79 @@
         v-for="link in choice.links"
         :key="link"
         :href="link.href"
-        :style="{color: choice.background }"
-      >{{ link.label }}</a>
+        :style="{ color: choice.background }"
+        >{{ link.label }}</a
+      >
     </div>
   </section>
-  <!-- <iframe
+  <iframe
     width="0"
     height="0"
-    src="https://www.youtube.com/embed/phOW-CZJWT0?autoplay=0"
+    src="https://www.youtube.com/embed/phOW-CZJWT0?autoplay=1"
     frameborder="0"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen
-  ></iframe>-->
+  ></iframe>
 </template>
 
 <script>
+import AppGithubCorner from '@/components/AppGithubCorner';
+
 export default {
+  components: {
+    AppGithubCorner
+  },
   data() {
     return {
       currentChoice: 0,
       choices: [
         {
           id: 1,
-          question: "Do you wanna rock right now?",
+          question: 'Do you wanna rock right now?',
           links: [
-            { href: "#2", label: "Yes" },
-            { href: "#6", label: "No" }
+            { href: '#2', label: 'Yes' },
+            { href: '#6', label: 'No' }
           ],
-          background: "#FF5F45"
+          background: '#FF5F45'
         },
         {
           id: 2,
-          question: "Did you come to get down?",
+          question: 'Did you come to get down?',
           links: [
-            { href: "#3", label: "Yes" },
-            { href: "#6", label: "No" }
+            { href: '#3', label: 'Yes' },
+            { href: '#6', label: 'No' }
           ],
-          background: "#0698ED"
+          background: '#0698ED'
         },
         {
           id: 3,
-          question: "Are you internationally known?",
+          question: 'Are you internationally known?',
           links: [
-            { href: "#6", label: "Yes" },
-            { href: "#4", label: "No" }
+            { href: '#6', label: 'Yes' },
+            { href: '#4', label: 'No' }
           ],
-          background: "#FC6C7C"
+          background: '#FC6C7C'
         },
         {
           id: 4,
-          question: "Are you known to rock a microphone?",
+          question: 'Are you known to rock a microphone?',
           links: [
-            { href: "#5", label: "Yes" },
-            { href: "#6", label: "No" }
+            { href: '#5', label: 'Yes' },
+            { href: '#6', label: 'No' }
           ],
-          background: "#FEC501"
+          background: '#FEC501'
         },
         {
           id: 5,
-          question: "You are Rob Base!",
-          links: [{ href: "#", label: "Start Over" }],
-          background: "black"
+          question: 'You are Rob Base!',
+          links: [{ href: '#', label: 'Start Over' }],
+          background: 'black'
         },
         {
           id: 6,
-          question: "You are not Rob Base",
-          links: [{ href: "#", label: "Start Over" }],
-          background: "black"
+          question: 'You are not Rob Base',
+          links: [{ href: '#', label: 'Start Over' }],
+          background: 'black'
         }
       ]
     };
@@ -85,7 +92,7 @@ export default {
 };
 </script>
 <style>
-@import "https://fonts.googleapis.com/css?family=Indie+Flower,Public+Sans&display=swap";
+@import 'https://fonts.googleapis.com/css?family=Indie+Flower,Public+Sans&display=swap';
 
 * {
   box-sizing: border-box;
@@ -112,14 +119,14 @@ section {
 .question {
   font-size: 50px;
   color: #fff;
-  font-family: "Indie Flower", cursive;
+  font-family: 'Indie Flower', cursive;
   margin-bottom: 20px;
 }
 .choice a {
   color: #fff;
   font-size: 30px;
   padding-right: 20px;
-  font-family: "Public Sans", sans-serif;
+  font-family: 'Public Sans', sans-serif;
   background-color: #fff;
   margin: 5px;
   text-align: center;
